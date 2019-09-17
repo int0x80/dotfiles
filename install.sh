1fd090763de0f45f763ae8c316e2501b52b9bd24
int0x80 <int0x80@users.noreply.github.com>
2019-09-17 08:25:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ls -1 "${wd}" | egrep -v '(COPYING|install.sh|README.md)' | while read target; do 
+ls -1 | egrep -v '(COPYING|install.sh|README.md)' | while read target; do 
   stow "${target}"
 done
