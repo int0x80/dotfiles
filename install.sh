b4a5569812d24740669ccfd5740bac8e284e0c78
int0x80 <int0x80@users.noreply.github.com>
2019-09-17 08:37:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-ls -1 | egrep -v '(COPYING|install.sh|README.md)' | while read target; do 
-  stow "${target}"
+wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+for app in bash screen tmux vim; do
+  find "${wd}/${app}" -type f | while read target; do
+    ln -s "${target}" "${HOME}"
+  done
 done
