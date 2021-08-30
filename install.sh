8918dd27702024745ec3a6c0d77c975db38834bb
int0x80 <int0x80@localhost>
2021-08-30 07:34:49 -0500

    file check

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 td="${HOME}/.config/terminator/"
 for app in bash screen tmux vim; do
   find "${wd}/${app}" -type f | while read target; do
-    mv -f "${HOME}/$(basename ${target})" "${HOME}/$(basename ${target})-$(date +%s)" 2>/dev/null
+    [ -f "${HOME}/$(basename ${target})" ] && mv -f "${HOME}/$(basename ${target})" "${HOME}/$(basename ${target})-$(date +%s)"
     ln -s "${target}" "${HOME}"
   done
 done
